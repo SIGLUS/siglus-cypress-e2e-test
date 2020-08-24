6e94df170ec2d7fd226d347a14142ab29ca2e878
swyin <swyin@thoughtworks.com>
2020-08-24 13:40:56 +0800

    [shangwei] #506 modify stockmanagement receive e2e test.

--- cypress/integration/receive/receive.spec.js
+++ cypress/integration/receive/receive.spec.js
@@ -4,13 +4,13 @@ import { getYesterday, getFutureDate } from '../../utils/date-util'
 
 describe('stockmanagement receive scenario',() => {
 
-  beforeEach(() =>{
+  before(() =>{
     cy.login(Cypress.env("username"),Cypress.env("password"))
   })
 
-  // afterEach(() => {
-  //   cy.logout()
-  // })
+  after(() => {
+    cy.logout()
+  })
 
   it('stockmanagement receive', () => {
     // go to receive products page
@@ -23,20 +23,21 @@ describe('stockmanagement receive scenario',() => {
         cy.get('tbody > .ng-isolate-scope > .ng-binding').should('contain', 'All Products')
         cy.get('#proceedButton').click().wait(10000).then(() => {
           // clear if draft existed
-          if (!cy.get('.pagination-info').should('contain', 'Showing no items')) {
-            cy.get('[ng-click="vm.removeDisplayItems()"]').click().then(() => {
-              cy.get('[ng-click="vm.confirm()"]').click()
-            })
-          }
+          cy.get('.pagination-info').then(element => {
+            if (element.text() !== 'Showing no items') {
+              cy.get('[ng-click="vm.removeDisplayItems()"]').click().then(() => {
+                cy.get('[ng-click="vm.confirm()"]').click()
+              })
+            }
+          })
         })
       })
     })
 
     // add products and fill value
-    const IBUPROFENO = 'Ibuprofeno'
-    const HIDRALAZINA = 'Hidralazina'
     const KIT = 'KIT AL/US'
-    const products = [IBUPROFENO, HIDRALAZINA, KIT]
+    const IBUPROFENO = 'Ibuprofeno'
+    const products = [KIT, IBUPROFENO, 'Hidralazina', 'Acetazolamida']
     cy.wrap(products).each((product, index) => {
       // add product
       cy.get('#select2-productSelect-container').click().then(() => {
@@ -44,12 +45,21 @@ describe('stockmanagement receive scenario',() => {
         cy.get('.add').click()
       })
       if (product !== KIT) {
-        // fill 'Lot Code', create new lot
-        cy.get(':nth-child(1) > :nth-child(3) > .stock-select-container > [ng-if="!enableInput"] > .form-control').click().then(() => {
-          cy.contains('Auto generate lot').click({force: true})
-        })
-        // fill 'Expiry Date'
-        cy.get(':nth-child(1) > :nth-child(4) > .input-control').type(getFutureDate())
+        if (product === IBUPROFENO) {
+          // fill 'Lot Code', create new lot
+          cy.get(':nth-child(1) > :nth-child(3) > .stock-select-container > [ng-if="!enableInput"] > .form-control').click().then(() => {
+            cy.contains('Auto generate lot').click({force: true})
+          })
+          // fill 'Expiry Date'
+          cy.get(':nth-child(1) > :nth-child(4) > .input-control').type(getFutureDate())
+        } else {
+          // fill 'Lot Code', use existed lot
+          cy.get('.custom-item-container').eq(0).click().wait(1000).then(() => {
+            cy.get('.adjustment-custom-item .option-list').then(() => {
+              cy.get('body>.adjustment-custom-item .option-list').children().eq(0).click()
+            })
+          })
+        }
       }
       // fill 'Received From'
       cy.get(':nth-child(1) > :nth-child(6) > .input-control').click().then(() => {
@@ -63,7 +73,7 @@ describe('stockmanagement receive scenario',() => {
         cy.get(':nth-child(1) > :nth-child(8) > .input-control').type(getYesterday())
       })
       // fill 'Documentation No.'
-      cy.get(':nth-child(1) > :nth-child(9) > .input-control').type('Doc-' + product)
+      cy.get(':nth-child(1) > :nth-child(9) > .input-control').click().type('Doc-' + product)
     })
 
     // click 'Save' button
@@ -76,9 +86,14 @@ describe('stockmanagement receive scenario',() => {
       cy.get('.open > .ng-isolate-scope > :nth-child(3) > .ng-binding').should('contain', 'Receive').click().then(() => {
         cy.url().should('contain','stockmanagement/receive')
         cy.get('#proceedButton').should('have.value', 'Continue').click().wait(10000).then(() => {
-          cy.get('.pagination-info').should('contain', 'Showing 3 item(s) out of 3 total')
+          cy.get('.pagination-info').should('contain', 'Showing 4 item(s) out of 4 total')
         })
       })
+    })
+
+    // click "Remove' button
+    cy.get(':nth-child(1) > :nth-child(10) > .danger').click().then(() => {
+      cy.get('.pagination-info').should('contain', 'Showing 3 item(s) out of 3 total')
     })
 
     // click 'Submit' button
